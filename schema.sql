f44014508836ba9338db9e1e9c64d01b04be99eb
mxweidmer <44995486+mxweidmer@users.noreply.github.com>
2019-03-27 22:03:13 -0700

    added seed data

--- schema.sql
+++ schema.sql
@@ -8,8 +8,8 @@ USE bamazonDB;
 CREATE TABLE products (
     id INT NOT NULL AUTO_INCREMENT,
     PRIMARY KEY (id),
-    product_name VARCHAR(100) NOT NULL,
-    department_name VARCHAR(100) NOT NULL,
+    product VARCHAR(100) NOT NULL,
+    department VARCHAR(100) NOT NULL,
     price DECIMAL(10,2) NOT NULL,
     quantity INT NOT NULL
 )
---
+++ seed.sql
@@ -0,0 +1,13 @@
+
+INSERT INTO products (product, department, price, quantity)
+
+VALUES
+("Intal i2", "Processors", 99.99, 100),
+("Intal i4", "Processors", 199.99, 75),
+("Intal i6", "Processors", 299.99, 50),
+("Namdia GRX 2000", "Graphics cards", 149.99, 70),
+("Namdia GRX 2000", "Graphics cards", 249.99, 50),
+("Namdia GRX 2000", "Graphics cards", 349.99, 30),
+("Implosive Design D2", "Cases", 74.99, 20),
+("Implosive Design D3", "Cases", 124.99, 20),
+("Implosive Design D4", "Cases", 174.99, 20)
