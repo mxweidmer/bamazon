2ac9ac0983a33ab66c589b89fe7e477ebff8e432
mxweidmer <44995486+mxweidmer@users.noreply.github.com>
2019-03-27 22:21:40 -0700

    created function to display product information with cli-table3(required)

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,6 @@
 
 var inquirer = require("inquirer");
-
+var Table = require("cli-table3");
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
@@ -16,4 +16,21 @@ var connection = mysql.createConnection({
 
 connection.connect(function (err) {
     if (err) throw err;
+    display();
 });
+
+function display() {
+    connection.query(
+        "SELECT * FROM products",
+        function (err, res) {
+            var table = new Table({
+                head: ["ID", "Product", "Department", "Price"]
+            });
+
+            for (var i = 0; i < res.length; i++) {
+                table.push([res[i].id, res[i].product, res[i].department, res[i].price])
+            }
+            console.log(table.toString());
+        }
+    )
+}
