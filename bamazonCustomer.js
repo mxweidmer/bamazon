0eeb247da872d8b669f9126072ec0e75da0c5640
mxweidmer <44995486+mxweidmer@users.noreply.github.com>
2019-03-27 22:55:49 -0700

    created main function which prompts user with inquirer

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -16,13 +16,58 @@ var connection = mysql.createConnection({
 
 connection.connect(function (err) {
     if (err) throw err;
-    display();
+    display(main);
+
 });
 
-function display() {
+function main() {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What is the ID of the item you would like to purchase? (q to quit)",
+            name: "item",
+            validate: function (value) {
+                if (isNaN(value) === false || value === "q") {
+                    return true;
+                }
+                return false;
+            }
+        }
+    ]).then(function (stepOne) {
+        if (stepOne.item === "q") {
+            console.log("Goodbye");
+            connection.end();
+        } else {
+            inquirer.prompt([
+                {
+                    type: "input",
+                    message: "How many would you like to buy?",
+                    name: "quantity",
+                    validate: function (value) {
+                        if (isNaN(value) === false || value === "q") {
+                            return true;
+                        }
+                        return false;
+                    }
+                }
+            ]).then(function (stepTwo) {
+                if (stepTwo.quantity === "q") {
+                    console.log("Goodbye");
+                    connection.end();
+                } else {
+                    makePurchase(stepOne.item, stepTwo.quantity);
+                }
+            })
+        }
+    })
+}
+
+function display(func) {
     connection.query(
         "SELECT * FROM products",
         function (err, res) {
+            if (err) throw err;
+
             var table = new Table({
                 head: ["ID", "Product", "Department", "Price"]
             });
@@ -31,6 +76,11 @@ function display() {
                 table.push([res[i].id, res[i].product, res[i].department, res[i].price])
             }
             console.log(table.toString());
+            func();
         }
     )
+}
+
+function makePurchase(item, quantity) {
+    console.log(item, quantity);
 }
